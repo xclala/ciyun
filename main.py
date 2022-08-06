3b45016d42ccbef8d5e9b18504e1bbf02d143e52
xclala <xingdongxd2021@163.com>
2022-08-06 18:21:22 +0800

    2022/08/06

--- main.py
+++ main.py
@@ -1,37 +1,37 @@
 from wordcloud import WordCloud
 from jieba import lcut,setLogLevel
-from win32ui import CreateFileDialog
-from win32con import OFN_OVERWRITEPROMPT, OFN_FILEMUSTEXIST
-from progress.bar import Bar
-file_open_type = 'All File(*.*)|*.*|''|'
-d = CreateFileDialog(1, None, None,
-                        OFN_OVERWRITEPROMPT | OFN_FILEMUSTEXIST,
-                        file_open_type)
-d.SetOFNInitialDir('C:/')
-d.DoModal()
-path = d.GetPathName()
-encoding = input("字符编码：")
-bar = Bar('生成中', max=7)
-if not encoding:
-    encoding = "utf-8"
-bar.next()
+from tkinter import *
+from tkinter.filedialog import askopenfile
+import tkinter.ttk
+
 try:
-    with open(path, "r", encoding=encoding) as f:
+    with askopenfile() as f:
         m = f.read()
-    bar.next()
-except FileNotFoundError:
-    print("您未选择文件，无法生成词云。")
-except UnicodeDecodeError:
-    print("字符编码错误。")
-w = WordCloud(height=1000, width=1900, stopwords={"的", "和", "与", "-", "——", "|"}, font_path=r"msyh.ttc")
-bar.next()
-setLogLevel(60)
-bar.next()
-m = " ".join(lcut(m))
-bar.next()
-w.generate(m)
-bar.next()
-w.to_file("output.png")
-bar.next()
-bar.finish()
-print("已生成output.png")
+except ValueError:
+    print("文件无法创建词云。")
+except (UnicodeEncodeError, UnicodeDecodeError):
+    print("文件编码错误，本程序只支持utf-8。")
+except Exception as e:
+    print(e)
+try:
+    root = Tk()
+    bar = tkinter.ttk.Progressbar(root, length=200)
+    bar.pack(side=TOP)
+    bar['maximum'] = 100
+    w = WordCloud(height=1000, width=1900, stopwords={"的", "和", "与", "-", "——", "|", "_"}, font_path=r"msyh.ttc")
+    setLogLevel(60)
+    bar['value'] = 25
+    root.update()
+    m = " ".join(lcut(m))
+    bar['value'] = 50
+    root.update()
+    w.generate(m)
+    bar['value'] = 75
+    root.update()
+    w.to_file("output.png")
+    bar['value'] = 100
+    root.update()
+    Label(root, text="已生成output.png").pack(side=BOTTOM)
+    mainloop()
+except Exception as e:
+    print(e)
